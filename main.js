b5619d654326ec61ca2ae221bae96ef5e37e2650
jaemin <jaemin4033@gmail.com>
2023-04-14 09:39:12 +0900

    finish

--- main.js
+++ main.js
@@ -28,3 +28,78 @@ for (let $ck of [...$checkbox]) {
         e.target.parentNode.classList.toggle('checked');
     })
 }
+
+// 용석 부분
+const $todoinput = document.querySelector('input.todo-input');
+console.log($todoinput);
+
+const $add = document.querySelector('.add');
+// console.log($add);
+
+const $todolist = document.querySelector('.todo-list');
+// console.log($todolist);
+
+const $delete = document.querySelectorAll('.delete');
+// console.log($delete);
+
+// const $checkbox = document.querySelectorAll('input[type="checkbox"]');
+// console.log($checkbox);
+
+
+$add.addEventListener('click', function () {
+    if ($todoinput.value === '') {
+      alert('할 일을 입력 해주세요');
+      return;
+    } else {
+      console.log($todoinput.value);
+      const $li = document.createElement('li');
+  
+      $li.innerHTML = `<input type="checkbox"/>
+      <p>${$todoinput.value}</p>
+      <i class="lnr lnr-undo modi"></i>
+      <i class="lnr lnr-cross-circle delete"></i>`;
+      $todolist.appendChild($li);
+      $todoinput.value = '';
+    }
+  });
+
+
+
+function createTodo(){
+    const $li = document.createElement('li');
+    $li.textContent = $todoinput.value;
+
+    const $delete = document.createElement('i');
+    $delete.className('.lnr lnr-cross-circle delete');
+    $li.appendChild($delete);
+
+
+
+    document.querySelector('.todo-list').appendChild($li);
+    $todoinput.value = '';
+};
+
+
+
+$add.addEventListener('click', createTodo);
+
+
+// const $ul = document.querySelector('.todo-list');
+$ul.addEventListener('click', e => {
+    
+    if(!e.target.matches('li > .delete')) {
+        return;
+    }
+    e.target.parentNode.classList.add('animation');
+    
+    const func = setTimeout(() => {
+    $ul.removeChild(e.target.parentNode);
+
+  }, 1000);
+
+
+
+    
+});
+
+
