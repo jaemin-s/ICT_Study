ac4419d7322318757d344f338b03f1c729784543
jmHome <jaemin4033@gmail.com>
2023-04-15 22:05:50 +0900

    홀짝 완

--- js/odd-or-even.js
+++ js/odd-or-even.js
@@ -18,15 +18,15 @@ const $startBtn = document.querySelector('.start'); // 시작하기
 const $reStartBtn = document.querySelector('.restart'); // 다시하기
 const $ruleBtn = document.querySelector('.rule.btn'); // 설명보기
 
-
+let num1 = 1,num2 = 1;
 
 $startBtn.onclick = function () {
 
 
     $startBtn.classList.add('blocked');
     // 랜덤 숫자 생성
-    const num1 = Math.floor(Math.random() * 6) + 1;
-    const num2 = Math.floor(Math.random() * 6) + 1;
+    num1 = Math.floor(Math.random() * 6) + 1;
+    num2 = Math.floor(Math.random() * 6) + 1;
 
     console.log('num1 = ' + num1);
     console.log('num2 = ' + num2);
@@ -42,23 +42,100 @@ $startBtn.onclick = function () {
         }
     , 800);
     setTimeout(() => $dice1.parentNode.classList.remove('hide'), 3200);
-    setTimeout(() => $reStartBtn.classList.remove('blocked'), 4000);
-    
+    setTimeout(() => {
+      $reStartBtn.classList.remove('blocked');
+      ooeResult();
+    }, 4000);
+}
 
+const $resultTxt = document.querySelector('.result-text');
+
+function ooeResult(){
+  const bTarget = document.querySelector('input[name="ooe-bet"]:checked');
+  const bType = bTarget.dataset.bettingType;
+  let flag = false;
+  let result;
+  if(bType === "ooe" ){
+    result = (num1+num2)%2===0?'even':'odd';
+    console.log(result);
+  }else if(bType === "sum"){
+    result = num1+num2;
+  }else if(bType === "minus"){
+    result = Math.abs(num1-num2);
+  }
+  $resultTxt.firstElementChild.textContent = `${num1} ${num2} ${(num1+num2)%2===0?'짝':'홀'}`;
+  if(result==bTarget.dataset.select){
+    $resultTxt.lastElementChild.textContent = `축하합니다`;
+    $resultTxt.parentNode.lastElementChild.classList.remove('hide');
+    flag = true;
+  }else{
+    $resultTxt.lastElementChild.textContent = `꽝`;
+  }
+  if(flag){
+    total += Number($bettingCash.value) + Number($bettingCash.value)*Number(bTarget.dataset.rate);
+  }else{
+    total -= Number($bettingCash.value);
+  }
+  $textinput2.value = total + ' 원';
+  $resultTxt.classList.remove('hide');
 
 }
 
+
 $reStartBtn.addEventListener('click',()=>{
     $cup.classList.remove('action');
-    $startBtn.classList.remove('blocked');
+    $startBtn.classList.add('blocked');
     $reStartBtn.classList.add('blocked');
+    [...$bettings].forEach(c =>{
+      c.classList.remove('blocked');
+    });
+    $resultTxt.classList.add('hide');
+    $resultTxt.parentNode.lastElementChild.classList.add('hide');
 });
 
 $ruleBtn.addEventListener('click',()=>{
     console.log($ruleBtn.lastElementChild);
     $ruleBtn.lastElementChild.classList.toggle('hide');
 });
+const $bettings = document.querySelectorAll('.bet-odd-even,.bet-cash,.btn-betting');
+const $bettingCash = document.getElementById('betting-cash');
+$betBtn.addEventListener('click',() =>{
+  if(!document.querySelector('input[name="ooe-bet"]:checked')){
+    alert('베팅할 항목을 선택하세요');
+}else if(total < $bettingCash.value){
+    alert('잔액이 부족합니다');
+}else{
+    if(confirm('이대로 베팅하시겠습니까?')){
+      [...$bettings].forEach(c =>{
+        c.classList.add('blocked');
+      });
+      $startBtn.classList.remove('blocked');
+    }
+}
+});
+const $cashPlus = document.querySelector('.bet-cash .bet-plus');
+const $castMinus = document.querySelector('.bet-cash .bet-minus');
 
+$cashPlus.addEventListener('click',()=>{
+  if(+$bettingCash.value>=200000){
+      alert('최고 베팅금액은 200000원입니다.');
+      return;
+  }
+  $bettingCash.value = +$bettingCash.value+10000;
+});
+$castMinus.addEventListener('click',()=>{
+  if(+$bettingCash.value<=10000){
+      alert('최소 베팅금액은 10000원입니다.');
+      return;
+  }
+  $bettingCash.value = +$bettingCash.value-10000;
+});
+
+
+
+
+
+////////////////
 
 
 
