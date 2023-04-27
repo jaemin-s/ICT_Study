1dee55c06551666c5dbbc5b1504711cfaf7fc533
kkang3617 <rkdgus3617@naver.com>
2023-04-27 18:17:11 +0900

    테이블 생성중

--- table.sql
+++ table.sql
@@ -5,6 +5,48 @@ CREATE TABLE m_users(
     user_job VARCHAR2(20) NOT NULL
 );
 
-DROP TABLE m_users;
+SELECT * FROM m_users;
 
-INSERT INTO m_users VALUES ("991212-1234567","홍길동","서울시 마포구","의사");
+CREATE TABLE prescriptions(
+    prescription_no NUMBER(6) PRIMARY KEY,
+    user_id VARCHAR2(20) REFERENCES m_users(user_id),
+    doctor_name VARCHAR2(20) NOT NULL,
+    p_date DATE DEFAULT sysdate NOT NULL,
+    day_medication NUMBER(3) NOT NULL
+);
+
+SELECT * FROM prescriptions;
+
+CREATE SEQUENCE prescriptions_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 100000
+    NOCYCLE
+    NOCACHE;
+
+
+
+CREATE TABLE products(
+    product_name VARCHAR2(40) PRIMARY KEY,
+    product_price NUMBER(10) DEFAULT 5000 NOT NULL,
+    company_name VARCHAR2(40) NOT NULL
+);
+
+
+CREATE SEQUENCE products_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 100000
+    NOCYCLE
+    NOCACHE;
+   
+SELECT * FROM products;
+
+
+CREATE TABLE ingredients(
+    product_name VARCHAR2(40),
+    ingredient VARCHAR2(40) NOT NULL
+);
+
+
+
