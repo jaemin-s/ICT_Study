c7cd3bbfe3bdb7c13abee8c42b416a97acb383c7
kkang3617 <rkdgus3617@naver.com>
2023-05-01 17:06:00 +0900

    서치서비스 작성중

--- Pm/src/com/java/menus/service/SearchService.java
+++ Pm/src/com/java/menus/service/SearchService.java
@@ -1,8 +1,11 @@
 package com.java.menus.service;
 
 import com.java.common.AppService;
+import com.java.menus.domain.*;
 
 import static com.java.view.AppUI.*;
+
+import java.util.List;
 
 public class SearchService implements AppService{
 
@@ -13,7 +16,7 @@ public class SearchService implements AppService{
 			int selection = inputInteger();
 			switch (selection) {
 			case 1:
-				
+				showSearchPrescription();
 				break;
 			case 2:
 				System.out.println("menu1-2");
@@ -27,6 +30,28 @@ public class SearchService implements AppService{
 		}//end while
 		
 	}//end start()
+	
+	
+	private List<Prescription> searchPrescription() {
+		System.out.println("\n### 처방이력을 조회할 환자의 주민등록번호를 입력하세요.");
+		System.out.println(">>> ");
+		String patientId = inputString();
+		return PrescriptionRepository.searchHistory(patientId);
+	}
+	
+	//처방이력검색
+	private void showSearchPrescription() {
+		List<Prescription> prescriptions = searchPrescription();
+		
+		if(!prescriptions.isEmpty()) {
+			System.out.println("\n========================== 처방이력 조회 결과 ==========================");
+			for(Prescription prescription : prescriptions) {
+				System.out.println(prescription);
+			}
+			
+		}
+		
+	}
 
 
 }//end class
