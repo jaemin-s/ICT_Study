1a5d4e7d4b9603d2338f900a1d4e29ae1413ec49
kkang3617 <rkdgus3617@naver.com>
2023-05-01 18:08:42 +0900

    서치서비스 작성

--- Pm/src/com/java/menus/service/SearchService.java
+++ Pm/src/com/java/menus/service/SearchService.java
@@ -2,13 +2,20 @@ package com.java.menus.service;
 
 import com.java.common.AppService;
 import com.java.menus.domain.*;
+import com.java.menus.repository.PrescriptionRepository;
+import com.java.menus.repository.DrugRepository;
 
+import static com.java.menus.repository.PrescriptionRepository.*;
+import static com.java.menus.repository.DrugRepository.*;
 import static com.java.view.AppUI.*;
 
 import java.util.List;
 
 public class SearchService implements AppService{
-
+	
+	private final PrescriptionRepository prescriptionRepository = new PrescriptionRepository();
+	private final DrugRepository drugRepository = new DrugRepository();
+	
 	@Override
 	public void start() {
 		while(true) {
@@ -19,7 +26,7 @@ public class SearchService implements AppService{
 				showSearchPrescription();
 				break;
 			case 2:
-				System.out.println("menu1-2");
+				showSearchDrug();
 				break;
 			case 3:
 				return;
@@ -29,18 +36,20 @@ public class SearchService implements AppService{
 			
 		}//end while
 		
-	}//end start()
+	}//end start()  입력변수
+
 	
-	
+
+	//처방이력 검색
 	private List<Prescription> searchPrescription() {
-		System.out.println("\n### 처방이력을 조회할 환자의 주민등록번호를 입력하세요.");
+		System.out.println("\n### 처방이력을 조회할 환자의 주민등록번호를 (-)를 포함하여 입력하세요.");
 		System.out.println(">>> ");
 		String patientId = inputString();
-		return PrescriptionRepository.searchHistory(patientId);
+		return prescriptionRepository.searchHistory(patientId);
 	}
 	
-	//처방이력검색
-	private void showSearchPrescription() {
+	//처방이력검색 후 조회결과 출력
+	private int showSearchPrescription() {
 		List<Prescription> prescriptions = searchPrescription();
 		
 		if(!prescriptions.isEmpty()) {
@@ -48,9 +57,36 @@ public class SearchService implements AppService{
 			for(Prescription prescription : prescriptions) {
 				System.out.println(prescription);
 			}
-			
+		} else {
+			System.out.println("\n### 조회 결과가 없습니다.");
 		}
 		
+		return prescriptions.size();
+	}
+	
+	//약 이름검색
+	private void searchDrug() {
+		String drugName = "";
+		System.out.println("\n### 조회할 의약품 이름을 입력하세요.(약품이름의 일부를 입력해도 됩니다.)");
+		System.out.println(">>> ");
+		drugName = "'%" + inputString() + "%'";
+		return drugRepository.searchDrug(drugName);
+	}
+	
+	//약 이름 검색 후 조회결과 출력
+	private int showSearchDrug() {
+		List<Drug> drugs = searchDrug();
+		
+		if(!drugs.isEmpty()) {
+			System.out.println("\n========================== 의약품 조회 결과 ==========================");
+			for(Drug drug : drugs) {
+				System.out.println(drug);
+			}
+		} else {
+			System.out.println("\n### 조회 결과가 없습니다.");
+		}
+		
+		return drugs.size();
 	}
 
 
