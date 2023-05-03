c45baa5238c87eef5042410182bce02ae313a572
jaemin <jaemin4033@gmail.com>
2023-05-03 16:32:57 +0900

    조회메뉴 출력화면 수정

--- Pm/src/com/java/menus/repository/DrugRepository.java
+++ Pm/src/com/java/menus/repository/DrugRepository.java
@@ -1,26 +1,25 @@
 package com.java.menus.repository;
 
 import java.sql.*;
-import java.util.*;
 
 import com.java.common.DataBaseConnection;
 import com.java.menus.domain.*;
 
 public class DrugRepository {
-	
+
 	private DataBaseConnection connection = DataBaseConnection.getInstance();
-	
+
 	//약 등록
 	public void insertDrug(Drug d) {
 		String sql = "INSERT INTO drugs VALUES (drugs_seq.NEXTVAL,?,?,?,?)";
-		
+
 		try(Connection conn = connection.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setString(1, d.getDrugName());
 			pstmt.setInt(2, d.getDrugPrice());
 			pstmt.setString(3, d.getIngredient());
 			pstmt.setString(4, d.getCompanyName());
-			
+
 			if(pstmt.executeUpdate() == 1) {
 				System.out.println("정상 등록되었습니다.");
 			} else {
@@ -30,32 +29,34 @@ public class DrugRepository {
 			e.printStackTrace();
 		}
 	}//end 약 등록
-		
+
 	//약 이름으로 찾기
-	public List<Drug> searchDrug(String keyword){
-		List<Drug> dList = new ArrayList<>();
+	public void searchDrug(String keyword){
 		String sql = "SELECT * FROM drugs WHERE drug_name LIKE "+keyword+"";
 		try(Connection conn = connection.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery()) {
-			while(rs.next()) {
-				Drug d = new Drug(
-						rs.getInt("drug_number"),
-						rs.getInt("drug_price"),
-						rs.getString("company_name"),
-						rs.getString("drug_name"),
-						rs.getString("ingredient")
-						
-						);
-				dList.add(d);
+			if(rs.next()) {
+				System.out.println("---------------------------------------------");
+				System.out.println("제품 번호 |    제품 이름    |제품 가격|    제품 성분    | 제조 회사");
+				while(rs.next()) {
+					System.out.print(
+							"    "+rs.getInt("drug_number")
+							+"\t| "+rs.getString("drug_name")
+							+"\t| "+rs.getInt("drug_price")
+							+"\t| "+rs.getString("ingredient")
+							+"\t| "+rs.getString("company_name")+"\n"
+							);
+				}
+			}else {
+				System.out.println("\n조회결과가 없습니다.");
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return dList;
 	}//end 처방전 조회
-	
+
 	//약 번호로 찾기
 	public String searchDrugName(int dNum) {
 		String dName = "";
@@ -63,7 +64,7 @@ public class DrugRepository {
 		try(Connection conn = connection.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery()) {
-			
+
 			if(rs.next()) {
 				dName = rs.getString("drug_name");
 			}
@@ -72,14 +73,14 @@ public class DrugRepository {
 		}
 		return dName;
 	}
-	
+
 	//수정
 	public void updateDrug(int dNum, String col) {
 		String sql = "UPDATE drugs SET "+col+" WHERE drug_number = ?";
 		try(Connection conn = connection.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
-				pstmt.setInt(1, dNum);
-			
+			pstmt.setInt(1, dNum);
+
 			if(pstmt.executeUpdate() == 1) {
 				System.out.println("정상 수정되었습니다.");
 			} else {
@@ -89,5 +90,5 @@ public class DrugRepository {
 			e.printStackTrace();
 		}
 	}//end 수정
-	
+
 }//end class
