b0cb0049650418386aa9141e164f45781bdde96e
kkang3617 <rkdgus3617@naver.com>
2023-05-03 18:41:41 +0900

    환자등록메뉴- 이미 등록된환자 판별

--- Pm/src/com/java/view/AppUI.java
+++ Pm/src/com/java/view/AppUI.java
@@ -45,7 +45,7 @@ public class AppUI {
 	
 	//2. 환자 관리 화면 출력
 	public static void PatientScreen() {
-        System.out.println("\n========= 환자 관리 =========");
+        System.out.println("\n========== 환자 관리 ==========");
         System.out.println("### 1. 신규 환자 등록");
         System.out.println("### 2. 처방전 작성");
         System.out.println("### 3. 첫 화면으로 가기");
@@ -55,7 +55,7 @@ public class AppUI {
 	
 	//제품 관리 화면 출력
 	public static void ProductScreen() {
-        System.out.println("\n========= 제품 관리 =========");
+        System.out.println("\n========== 제품 관리 ==========");
         System.out.println("### 1. 제품 등록하기");
         System.out.println("### 2. 제품 정보수정");
         System.out.println("### 3. 첫 화면으로 가기");
