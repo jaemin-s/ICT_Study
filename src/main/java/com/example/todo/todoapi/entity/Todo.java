fdbef794214d23057aa081c697b990a5f0b3c1b9
jaemin <jaemin4033@gmail.com>
2023-06-20 17:29:16 +0900

    todo start

---
+++ src/main/java/com/example/todo/todoapi/entity/Todo.java
@@ -0,0 +1,33 @@
+package com.example.todo.todoapi.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString @EqualsAndHashCode(of = "todoId")
+@Entity
+@Table(name = "tbl_todo")
+@Builder
+public class Todo {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid",strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    private boolean done;
+
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+
+
+}
